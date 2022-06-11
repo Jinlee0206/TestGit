d7f8c67955863a2a4a01b80b24655f796f390c1c
Jinlee0206 <ljs4784@gmail.com>
2022-06-12 00:12:33 +0900

    20220612_Commit

---
+++ Inflearn/68.최소비용(DFS_가중치방향그래프인접리스트).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, ch[30], cost = -2147000000;
+
+vector<int> map[30];
+
+void DFS(int vertex, int sum)
+{
+
+}
+
+int main()
+{
+	int M;
+	cin >> N >> M;
+
+	for (int i = 1; i <= M; i++)
+	{
+		
+	}
+
+
+	return 0;
+}
